f971c72b0e201efecaf075042fd5687f61697f22
Sameep Vani <sameepvani@Sameeps-Laptop.local>
2024-05-08 12:35:23 -0700

    patch: minor issue fixed
    prob_fix: still under probfix section

--- nfn/layers/encoding.py
+++ nfn/layers/encoding.py
@@ -85,9 +85,7 @@ class PositionalEncoding(nn.Module):
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0, 0::2] = torch.sin(position * div_term)
         pe[:, 0, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)Container(
-            child: 
-        )
+        pe = pe.unsqueeze(0).transpose(0, 1)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
